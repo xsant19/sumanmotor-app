9b3e4137721fdfae086245738513bbd0deb84874
Antara <Sumantara13@gmail.com>
2023-07-11 19:25:09 +0800

    register fix

--- resources/views/home/components/pages/register-home.blade.php
+++ resources/views/home/components/pages/register-home.blade.php
@@ -51,17 +51,7 @@
                     placeholder="Masukkan email anda" required value="{{ old('email') }}">
             </div>
         </div>
-        <div class="w-full lg:w-6/12 px-4">
-            <div class="relative w-full mb-3">
-                <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
-                    Username
-                </label>
-                <input type="text" id="username" name="username"
-                    class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    placeholder="Masukkan Username" required value="{{ old('usernama') }}">
-            </div>
-        </div>
-        <div class="w-full lg:w-6/12 px-4">
+        <div class="w-full lg:w-12/12 px-4">
             <div class="relative w-full mb-3">
                 <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
                     No Telepon
@@ -72,10 +62,6 @@
             </div>
         </div>
     </div>
-    {{-- <hr class="mt-6 border-b-1 border-blueGray-300"> --}}
-    {{-- <h6 class="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
-                            Contact Information
-                        </h6> --}}
     <div class="flex flex-wrap">
         <div class="w-full lg:w-12/12 px-4">
             <div class="relative w-full mb-3">
